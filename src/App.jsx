dc293a2b39bf0de6ea570642a905ea81ad5a8916
manoj <manojthulung03@gmail.com>
2024-10-01 11:16:44 +0545

    Feat: Added new pages and Added dynamic title in header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
+import OrderManagement from "./pages/OrderManagement";
+import ProductList from "./pages/ProductList";
+import AddProducts from "./pages/AddProducts";
+import Brand from "./pages/Brand";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/order-management" element={<OrderManagement />} />
+            <Route path="/brand" element={<Brand />} />
+            <Route path="/add-products" element={<AddProducts />} />
+            <Route path="/product-list" element={<ProductList />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,10 +1,16 @@
+import { useLocation } from "react-router-dom";
+import { formatHeaderText } from "../../lib/helper";
 import Notification from "./Notification";
 import Profile from "./Profile";
 
 const Header = () => {
+  const route = useLocation();
+
+  const headerTitle = formatHeaderText(route.pathname);
+
   return (
     <header className="flex justify-between">
-      <h1 className="text-2xl font-bold items-center">Dashboard</h1>
+      <h1 className="text-2xl font-bold items-center">{headerTitle}</h1>
       <div className="flex gap-4 items-center">
         <Notification />
         <Profile />
